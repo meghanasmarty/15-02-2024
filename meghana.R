31698a85fde57c20465090c23d300d8082b16f3a
meghanasmarty <159746285+meghanasmarty@users.noreply.github.com>
2024-02-15 10:45:26 +0530

    first commit today

---
+++ meghana.R
@@ -0,0 +1,51 @@
+IN-LAB:
+  
+  Task-1:  Sort elements by their frequency and index
+Given an integer array, sort its element by their frequency and index. i.e., if two elements have different frequencies, then the one which has more frequency should come first; otherwise, the one which has less index should come first.
+For example,    Input : [3, 3, 1, 1, 1, 8, 3, 6, 8, 7, 8]        Output: [3, 3, 3, 1, 1, 1, 8, 8, 8, 6, 7]
+
+Task-2:  In-place merge two sorted arrays
+Given two sorted arrays, X[] and Y[] of size m and n each, merge elements of X[] with elements of array Y[] by maintaining the sorted order, i.e., fill X[] with the first m smallest elements and fill Y[] with remaining elements.
+Do the conversion in-place and without using any other data structure.
+For example,   
+Input:   X[] = { 1, 4, 7, 8, 10 }    Y[] = { 2, 3, 9 } 
+Output: X[] = { 1, 2, 3, 4, 7 }Y[] = { 8, 9, 10 }
+
+Task-3: Find minimum platforms needed to avoid delay in the train arrival
+Given a schedule containing the arrival and departure time of trains in a station, find the minimum number of platforms needed to avoid delay in any train’s arrival.
+For example,
+Trains arrival   = { 2.00, 2.10, 3.00, 3.20, 3.50, 5.00 }
+Trains departure = { 2.30, 3.40, 3.20, 4.30, 4.00, 5.20 } 
+The minimum platforms needed is 2  
+The train arrived at 2.00 on platform 1
+The train arrived at 2.10 on platform 2
+The train departed at 2.30 from platform 1
+The train arrived at 3.00 on platform 1
+The train departed at 3.20 from platform 1
+The train arrived at 3.20 on platform 1
+The train departed at 3.40 from platform 2
+The train arrived at 3.50 on platform 2
+The train departed at 4.00 from platform 2
+The train departed at 4.30 from platform 1
+The train arrived at 5.00 on platform 1
+The train departed at 5.20 from platform 1
+
+Task-4: Merge `M` sorted lists each containing `N` elements
+Given m sorted lists, each containing n elements, print them efficiently in sorted order.
+
+For example,   Input: 5 sorted lists of fixed size 4
+[10, 20, 30, 40]
+[15, 25, 35, 45]
+[27, 29, 37, 48]
+[32, 33, 39, 50]
+[16, 18, 22, 28] 
+Output: [10, 15, 16, 18, 20, 22, 25, 27, 28, 29, 30, 32, 33, 35, 37, 39, 40, 45, 48, 50]
+POST-LAB:
+  
+  1.	https://leetcode.com/problems/h-index/description/
+  2.	https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/description/
+  3.	https://leetcode.com/problems/heaters/description/
+  4.	https://leetcode.com/problems/custom-sort-string/description/
+  
+  
+  
