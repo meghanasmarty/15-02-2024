a20c6dd564919a6a9c8ee7e759603578babcd0d9
meghanasmarty <kollurimeghana1@gmail.com>
2024-02-22 11:43:34 +0530

    vectors

--- sample.R
+++ sample.R
@@ -1,2 +1,17 @@
 numeric_vector <- c(1, 2, 3, 4, 5)
 numeric_vector[3]
+character_vector <- c("apple", "orange", "banana")
+logical_vector <- c(TRUE, FALSE, TRUE)
+character_vector[2]  
+result_multiplication <- vec1 * vec2
+print(result_multiplication)
+result_mean <- mean(numeric_vector)
+print(result_mean)
+# Sum of elements
+result_sum <- sum(numeric_vector)
+print(result_sum)
+# Sum of elements
+result_sum <- sum(numeric_vector)
+print(result_sum)
+
+
